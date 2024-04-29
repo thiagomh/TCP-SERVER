48c65ea5c14aa885b472752c38bba7cb7eb8b334
Thiago Henrique <thiagomhenrique@gmail.com>
2024-04-29 19:07:14 -0300

    thread server

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+
+
+def main():
+
+      return 
+
+if __name__ == "__main__":
+      main()
--- server.py
+++ server.py
@@ -1,26 +1,53 @@
 import socket
+#import struct
+from _thread import *
+import threading
 
-IP = ""
+IP = '127.0.0.1'
 PORT = 4455
 ADDR = (IP, PORT)
 BUFFER = 1024
 
-def tcp_segment():
+class tcp_segment:
       # source_port(2 bytes) - dest_port(2 bytes) - seq_num(4 bytes)
       # ack_num(4 bytes) - rcv_window(2 bytes) - checksum(2 bytes)
-      # header_len(4 bits) - rsv(6 bits) - red_flag (6 bits)  
+      # header_len; ack; rst; syn; fin (1 byte)   
+
       MSS = 1000
-      HEADER_FORMAT = ''
+      HEADER_FORMAT = 'H H I I H H b'
+
+      def __init__(self, source_port, dest_port, seq_num, ack_num, rcv_window, data, FIN) -> None:
+            self.source_port = source_port
+            self.dest_port = dest_port
+            self.seq_num = seq_num
+            self.ack_num = ack_num
+            self.rcv_window = rcv_window
+
+            self.ack = 1
+            self.fin = FIN
+            self.syn = 0
+
+            self.data = data
+
+def client_request():
+      return 
 
 def main():
       # Criando socket TCP 
-      # Dominio - AF_INET (IPv4) 
-      # SOCK_STREAM - Tipo de comunição; fluxo de bytes sem delimitação; full duplex
       serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       # Associando socket a um endereço(IP+PORTA)
       serverSocket.bind(ADDR)
       # Abre a porta na qual no servidor vai aguardar conexões TCP
       serverSocket.listen()
+      print(f"Server running {ADDR}")
+
+      while True:
+            clientSocket, client_addr = serverSocket.accept()
+
+            print("Connected to:", client_addr[0], ":", client_addr[1])
+
+            start_new_thread(client_request, (clientSocket))
+      
 
 if __name__ == "__main__":
       main()
